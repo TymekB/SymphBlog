bcea47bdd46a7d91d0d7ee67c39776c6d4a5d9b9
TymekB <baran.tymoteusz@gmail.com>
2018-05-06 10:00:24 +0200

    added creating comments

--- src/Controller/CommentsController.php
+++ src/Controller/CommentsController.php
@@ -2,13 +2,49 @@
 
 namespace App\Controller;
 
-use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Comment;
+use App\Entity\Post;
+use App\Form\CommentType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 
 class CommentsController extends Controller
 {
-    public function create()
+    public function create(Request $request, AuthorizationCheckerInterface $authChecker, $postId)
     {
-        return $this->render('comments/create.html.twig');
+        $comment = new Comment();
+        $post = $this->getDoctrine()->getRepository(Post::class)->find($postId);
+
+        if(!$post) {
+            throw $this->createNotFoundException("Post " . $postId . " not found!");
+        }
+
+        $form = $this->createForm(CommentType::class, $comment);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+
+            if(!$authChecker->isGranted("IS_AUTHENTICATED_FULLY")) {
+                $this->addFlash('danger', 'You must be logged to add a comment!');
+            } else {
+                if($authChecker->isGranted('ROLE_ADMIN')) {
+                    $comment->setAdmin($this->getUser());
+                } else {
+                    $comment->setUser($this->getUser());
+                }
+
+                $comment->setCreatedAt(new \DateTime());
+                $comment->setPost($post);
+
+                $entityManager = $this->getDoctrine()->getManager();
+                $entityManager->persist($comment);
+                $entityManager->flush();
+
+                return $this->redirectToRoute("post_show", ['id' => $post->getId()]);
+            }
+        }
+
+        return $this->render('comments/create.html.twig', ['form' => $form->createView(), 'post' => $post]);
     }
 }
--- src/Form/CommentType.php
+++ src/Form/CommentType.php
@@ -10,15 +10,12 @@ namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 
-class PostType extends AbstractType
+class CommentType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder
-            ->add('header', TextType::class)
-            ->add('body', TextareaType::Class, ['attr' => ['rows' => '15']]);
+        $builder->add('body', TextareaType::class);
     }
 }
