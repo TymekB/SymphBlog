3b7a79f8a93aecd05e76d5e5a61a2da4d037194d
TymekB <baran.tymoteusz@gmail.com>
2018-05-17 20:34:52 +0200

    added editing comments

---
+++ src/Controller/CommentsController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Comment;
+use App\Form\CommentType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class CommentsController extends Controller
+{
+    public function edit($id, Request $request)
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $comment = $entityManager->getRepository(Comment::class)->find($id);
+
+        if(!$comment || $comment->getUser()->getId() != $this->getUser()->getId()) {
+            throw $this->createNotFoundException("Comment " . $id . " not found!");
+        }
+
+        $form = $this->createForm(CommentType::class, $comment);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            $this->addFlash("success", "Comment edited!");
+
+            return $this->redirectToRoute('post_show', ['id' => $comment->getPost()->getId()]);
+        }
+
+        return $this->render('comments/edit.html.twig', ['form' => $form->createView()]);
+    }
+}
