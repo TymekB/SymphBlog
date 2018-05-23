bee8599c05aa201ec1552309af575962447f8a98
TymekB <baran.tymoteusz@gmail.com>
2018-05-23 20:44:41 +0200

    added HTMLPurifier

--- src/Form/CommentType.php
+++ src/Form/CommentType.php
@@ -8,14 +8,26 @@
 
 namespace App\Form;
 
+use App\Form\DataTransformer\TextToCleanHtmlTransformer;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 
 class CommentType extends AbstractType
 {
+    /**
+     * @var TextToCleanHtmlTransformer
+     */
+    private $textToCleanHtmlTransformer;
+
+    public function __construct(TextToCleanHtmlTransformer $textToCleanHtmlTransformer)
+    {
+        $this->textToCleanHtmlTransformer = $textToCleanHtmlTransformer;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder->add('body', TextareaType::class);
+        $builder->get('body')->addModelTransformer($this->textToCleanHtmlTransformer);
     }
 }
--- src/Form/PostType.php
+++ src/Form/PostType.php
@@ -8,6 +8,7 @@
 
 namespace App\Form;
 
+use App\Form\DataTransformer\TextToCleanHtmlTransformer;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -15,10 +16,23 @@ use Symfony\Component\Form\FormBuilderInterface;
 
 class PostType extends AbstractType
 {
+    /**
+     * @var TextToCleanHtmlTransformer
+     */
+    private $textToCleanHtmlTransformer;
+
+    public function __construct(TextToCleanHtmlTransformer $textToCleanHtmlTransformer)
+    {
+        $this->textToCleanHtmlTransformer = $textToCleanHtmlTransformer;
+    }
+
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
             ->add('header', TextType::class)
             ->add('body', TextareaType::Class, ['attr' => ['id' => 'article-ckeditor']]);
+
+        $builder->get('body')->addModelTransformer($this->textToCleanHtmlTransformer);
     }
 }
